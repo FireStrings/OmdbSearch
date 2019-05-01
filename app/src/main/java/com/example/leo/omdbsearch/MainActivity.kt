0f02963a9b6ddbe3d471e953b2106c2e2707f5e4
leo <liradiniz89@gmail.com>
2019-05-01 11:01:38 -0300

    Construção do layout e inicio do Backend

--- app/src/main/java/com/example/leo/omdbsearch/MainActivity.kt
+++ app/src/main/java/com/example/leo/omdbsearch/MainActivity.kt
@@ -1,33 +1,86 @@
-package com.example.leo.omdbsearch
+package com.example.leo.ombdsearch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.example.leo.omdbsearch.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
-    object codigosMensagen {
-        // Constante usada para envio de mensagens ao Handler
-        val RESPOSTA_TRADUCAO = 0
-    }
-    // Idiomas de origem e destino. Dependem da API do Oxford Dict.
-    val idiomas = listOf("pt", "en")
-    // Handler da thread de UI
-    lateinit var tradutoHandler: TradutoHandler
-    inner class TradutoHandler: Handler() {
-        override fun handleMessage(msg: Message?) {
-            if (msg?.what == codigosMensagen.RESPOSTA_TRADUCAO) {
-                // Alterar o conteúdo do TextView
-                traduzidoTv.text = msg?.obj.toString()
-            }
-        }
-    }
+
+class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val t = Teste(this)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        displayScreen(-1)
     }
 
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+
+        }
+
+    }
+
+    fun  displayScreen (id: Int){
+        val fragment = when (id){
+
+            R.id.byName -> {
+                SearchByNameActivity ()
+            }
+
+            R.id.byId -> {
+                SearchByIdActivity ()
+            }
+
+            else -> {
+                Main2Activity()
+            }
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.relativelayout, fragment)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        displayScreen(item.itemId)
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
 }
---
+++ app/src/main/java/com/example/leo/omdbsearch/Teste.kt
@@ -0,0 +1,96 @@
+package com.example.leo.ombdsearch
+
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import model.Resposta
+import org.json.JSONException
+import org.json.JSONObject
+
+class Teste(val mainActivity: MainActivity) {
+
+    fun main() {
+        Log.v("TESTE", "INIT MAIN")
+        val filaRequisicaoTraducao: RequestQueue = Volley.newRequestQueue(mainActivity)
+// Monta a requisição que será colocada na fila. Esse objeto é uma instância de uma classe anônima
+        var traducaoJORequest: JsonObjectRequest =
+            object : JsonObjectRequest(
+                Request.Method.GET, // Método HTTP de requisição
+                "http://www.omdbapi.com/?t=Guardians&apikey=b37f9b95", // URL
+                null, // Objeto de requisição - somente em POST
+                RespostaListener(), // Listener para tratar resposta
+                ErroListener() // Listener para tratar erro
+            ) {
+                // Corpo do objeto
+                // Sobreescrevendo a função para passar cabeçalho na requisição
+                override fun getHeaders(): MutableMap<String, String> {
+                    // Cabeçalho composto por Map com app_id, app_key e seus valores
+                    var parametros: MutableMap<String, String> = mutableMapOf()
+//                    parametros.put("apikey", "b37f9b95")
+//                    parametros.put("i", "tt3896198")
+                    Log.v("TESTE", "PARAMETROS")
+                    return parametros
+                }
+            }
+// Adiciona a requisição a fila
+        filaRequisicaoTraducao.add(traducaoJORequest)
+        Log.v("TESTE", traducaoJORequest.url)
+        Log.v("TESTE", traducaoJORequest.bodyContentType)
+        Log.v("TESTE", traducaoJORequest.headers.toString())
+
+
+    }
+
+    inner class RespostaListener : Response.Listener<JSONObject> {
+        override fun onResponse(response: JSONObject?) {
+            try {
+
+                Log.v("TESTE", "NO RESPOSTA")
+                Log.v("TESTE", response.toString())
+
+//                // Cria um objeto Gson que consegue fazer reflexão de um Json para Data Class
+                val gson: Gson = Gson()
+//                // Reflete a resposta (que é um Json) num objeto da classe Resposta
+                val resposta: Resposta = gson.fromJson(response.toString(), Resposta::class.java)
+
+
+                Log.v("TESTE", resposta.results!!.size.toString())
+//                // StringBuffer para armazenar o resultado das traduções
+//                var traduzidoSb = StringBuffer()
+//                // Parseando o objeto e adicionando as traduções ao StringBuffer, O(N^5)
+//                resposta.results?.forEach {
+//                    it?.lexicalEntries?.forEach {
+//                        it?.entries?.forEach {
+//                            it?.senses?.forEach {
+//                                it?.translations?.forEach {
+//                                    traduzidoSb.append("${it?.text}, ")
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                // Enviando as tradução ao Handler da thread de UI para serem mostrados na tela
+//                mainActivity.tradutoHandler.obtainMessage(
+//                    RESPOSTA_TRADUCAO,
+//                    traduzidoSb.toString().substringBeforeLast(',')
+//                ).sendToTarget()
+            } catch (jse: JSONException) {
+//                mainActivity.mainLl.snackbar("Erro na conversão JSON")
+            }
+        }
+    }
+
+    // Trata erros na requisição ao WS
+    inner class ErroListener : Response.ErrorListener {
+        override fun onErrorResponse(error: VolleyError?) {
+            Log.v("TESTE", error.toString())
+            //mainActivity.mainLl.snackbar("Erro na requisição: ${error.toString()}")
+        }
+    }
+
+}
